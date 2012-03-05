fc6b4c77719d8f10177ea7e9d856cd8c0b4a1ac7
Samir Jindel <sjindel@cmu.edu>
2012-03-05 07:38:02 -0500

    Fixed mem leak in test.

--- test.c
+++ test.c
@@ -7,14 +7,20 @@ int main(int argc, char** argv)
 {
     // Initialize a sim_desc.
 
-    char* trace = calloc(100,sizeof(char));
-    trace[99] = 1;
+    char* init = calloc(100,sizeof(char));
+    init[99] = 1;
 
-    sim_desc pca = {1, 110, 100, 100, trace};
+    sim_desc pca = {1, 110, 100, 100, init};
 
     simulation* result = run(&pca, time(0));
 
     print_simulation(result);
+
+    free(init);
+
+    free(result->trace);
+
+    free(result);
 
     return 0;
 }
