cb0d0c9b41e4111e1157d25b484d5752650286a2
Samir Jindel <sjindel@sjindel.org>
2012-04-22 06:16:32 -0400

    Migrated Core.hs to Seqence library, giving dramatic perf. improvements.

--- hs/Core.hs
+++ hs/Core.hs
@@ -1,4 +1,6 @@
 import Data.List
+import qualified Data.Sequence as S
+import qualified Data.Foldable as F
 
 type Cell = Int
 type Prob = Double
@@ -26,6 +28,10 @@ binToDec = binToDec' 0
 dive :: Integral n => n -> (a -> [a]) -> ([a] -> [a]) -> [a] -> [a]
 dive 0 _ _ i = i
 dive n g f i = dive (n-1) g f $ f $ concat $ map g i
+
+diveSeq :: Integral n => n -> (a -> S.Seq a) -> (S.Seq a -> S.Seq a) -> S.Seq a -> S.Seq a
+diveSeq 0 _ _ i = i
+diveSeq n g f i = diveSeq (n-1) g f $ f $ F.foldr (S.><) S.empty $ fmap g i
 
 trace :: Integral n => n -> (a -> [a]) -> ([a] -> [a]) -> [a] -> [[a]]
 trace 0 _ _ i = [i]
@@ -58,6 +64,10 @@ update bg p inst sch = Inst ((probability p sch) * (instProb inst))
       eval u (p,c,n) = if u then evaluateRule (p,c,n) else c
       newConfig = zipWith eval sch (triplize (bg:bg:config ++ [bg,bg]))
 
+generateSchedulesSeq :: Int -> S.Seq [Bool]
+generateSchedulesSeq 0 = S.singleton []
+generateSchedulesSeq n =  (fmap (False:) rest) S.>< (fmap (True:) rest)
+    where rest = generateSchedulesSeq (n-1) :: S.Seq [Bool]
 
 generateSchedules 0 = [[]]
 generateSchedules n =  (map (False:) rest) ++ (map (True:) rest)
@@ -73,10 +83,20 @@ meld :: Inst -> [Inst] -> Inst
 meld i is = Inst probSum (instStep i) (instConfig i)
     where probSum = (instProb i) + (sum $ map instProb is)
 
+meldSeq :: Inst -> S.Seq Inst -> Inst
+meldSeq i is = Inst probSum (instStep i) (instConfig i)
+    where probSum = (instProb i) + (F.sum $ fmap instProb is)
+
 combine :: [Inst] -> [Inst]
 combine [] = []
 combine (i:is) = (meld i same):(combine rest)
     where (same,rest) = partition (\x -> (instConfig x) == (instConfig i)) is
+
+combineSeq :: S.Seq Inst -> S.Seq Inst
+combineSeq is = case S.viewl is of
+                  S.EmptyL -> S.empty
+                  i S.:< is -> (meldSeq i same) S.<| (combineSeq rest)
+                      where (same,rest) = S.partition (\x -> (instConfig x) == (instConfig i)) is
 
 expected insts = sum $ map (\i -> (instProb i)*
                                   (fromIntegral $ sum $ instConfig i)) insts
@@ -91,11 +111,19 @@ continue bg p inst = combine $ map ((trimInst bg).(update bg p inst)) (generateS
     where len2 = (length $ instConfig inst) + 2
           trimInst c i = Inst (instProb i) (instStep i) (trim c $ instConfig i)
 
+continueSeq :: Cell -> Prob -> Inst -> S.Seq Inst
+continueSeq bg p inst = combineSeq $ fmap ((trimInst bg).(update bg p inst)) (generateSchedulesSeq len2)
+    where len2 = (length $ instConfig inst) + 2
+          trimInst c i = Inst (instProb i) (instStep i) (trim c $ instConfig i)
+
 toPair :: Inst -> String
 toPair (Inst p s c) = (show s) ++ "," ++ (show p)
 
 --main = putStrLn $ concat $ intersperse "\n" $ map toPair $
 --       concat $ trace 9 (continue 0 0.95) combine [(Inst 1 0 [1])]
 
+--main = putStrLn $ concat $ intersperse "\n" $ map show $
+--       concat $ trace 9 (continue 0 0.95) combine [(Inst 1 0 [1])]
+
 main = putStrLn $ concat $ intersperse "\n" $ map show $
-       concat $ trace 9 (continue 0 0.95) combine [(Inst 1 0 [1])]
+       concat $ F.toList $ trace 9 (continue 0 0.95) combine [(Inst 1 0 [1])]
